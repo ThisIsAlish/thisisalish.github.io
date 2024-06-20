66665939054ed6b87236a77e7be3c3eebb63850f
irekahsila <ali@alish.me>
2024-06-20 19:42:55 +0930

    Added _site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,26 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1718876646',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/resume/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
